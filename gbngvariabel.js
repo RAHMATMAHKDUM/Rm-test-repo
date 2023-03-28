a6c222efc5fb55a70f1f3c51a4e186a3bb2ed618
RAHMATMAHKDUM <rahmatmakdum@gmail.com>
2023-03-28 10:11:13 +0700

    gbang variavel js

---
+++ gbngvariabel.js
@@ -0,0 +1,11 @@
+var lastname;
+lastname = "Rahmat Mahkdum";
+console.log(lastname);
+
+let umur =17;
+console.log(umur);
+const tanggallahir = "06-10-2005";
+console.log(tanggallahir);
+document.write("nama :"+lastname+"</br>");
+document.write("umur:"+umur+"</br>");
+document.write("tanggallahir:"+ tanggallahir+"</br>");
