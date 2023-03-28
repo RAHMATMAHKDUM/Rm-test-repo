07c39ca50d6ce6ff09cce6c65bf3747c7c21b888
RAHMATMAHKDUM <rahmatmakdum@gmail.com>
2023-03-28 08:39:44 +0700

    menambahkan tgs komtar js

---
+++ komentar.js
@@ -0,0 +1,7 @@
+// ini merupakan komentar 1 baris,kode dibawh tidak akan dijalankan
+console.log("Hello Word");
+// concole.log("hallo");
+
+/* ini merupakan kommentar dengan lebih dari satu baris teks apapun yang berada disini akan
+dijadikan komentar.Ketika menggunakan ini,jangan lupa untuk menutupnya
+*/
